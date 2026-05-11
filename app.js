37fddaeac8d40798a7e2519f9dad17b3d44b3868
emmanuelobieze <emmanuelobieze468@gmail.com>
2026-05-11 05:59:44 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+alert('conected')
+
+let number = 5;
+
+//Number data-type
+let num1 = false;
+// console.log(type of num1);
+
+// console.log (number(num1));
+// console.log (type number(num1));
+
+
+let decimal = 10.534;
+console.log(parselnt(decimal));
+
+            
+// `1 + 1
+//      3 * 3
+// parseFloat("839");
+// praselnt("33.3333333");
+// 12 + "12"
+// "12" + 12
+// "12" + 12
+
+// let firstStr = "hello", secondStr = "world"
+
+// let sumstring = + " " + secondStr
+
+// let conncatStr = firstStr.concat(secondStr);
+
+// let tempStr = '${ firstStr } ${secondStr}';
+
+// console.log(concatstr)
+// console.log(sumstring)
+// console.log(tempStr)
+
+// let school = "myteacher";
+// console.log(school.indexOf('e'));
